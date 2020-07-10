0e542dbdc6af97f7f15c5af90e466f12de6dd871
Yasir Malik <yasirrafiq786@gmail.com>
2020-07-10 17:48:00 +1000

    removed datetime from order as it is already there by default

--- db/migrate/20200710073727_create_orders.rb
+++ db/migrate/20200710073727_create_orders.rb
@@ -1,7 +1,6 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-      t.datetime :created_at
       t.string :order_name
       t.references :customer, null: false, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_10_065401) do
+ActiveRecord::Schema.define(version: 2020_07_10_074457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,46 @@ ActiveRecord::Schema.define(version: 2020_07_10_065401) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "customers", force: :cascade do |t|
+    t.string "user_id"
+    t.string "login"
+    t.string "password"
+    t.string "name"
+    t.bigint "company_id", null: false
+    t.integer "credit_card"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_customers_on_company_id"
+  end
+
+  create_table "deliveries", force: :cascade do |t|
+    t.bigint "order_item_id", null: false
+    t.integer "delivered_quantity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_item_id"], name: "index_deliveries_on_order_item_id"
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.float "price_per_unit"
+    t.integer "quantity"
+    t.string "product"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "order_name"
+    t.bigint "customer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
+  end
+
+  add_foreign_key "customers", "companies"
+  add_foreign_key "deliveries", "order_items"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "orders", "customers"
 end
